8b271f7d118945c5ee86ba5dd9dec72588c07f8b
LeftCircle <rcato60@gmail.com>
2024-05-01 17:31:40 -0600

    Started plugin for shared gdscript files

---
+++ RollbackAuthClient/RefCounted/Netcode/Server/ClientInitializer.gd
@@ -0,0 +1,32 @@
+extends RefCounted
+class_name ClientInitializer
+
+var local_ip = "127.0.0.1"
+var server_ip = "20.185.61.230"
+var port : int = 28836
+var max_players : int = 100
+var max_channels : int = 3
+
+func init_client(scene_tree : SceneTree, network : ENetMultiplayerPeer, server_api : SceneMultiplayer, server_node : Node) -> void:
+	_connect_network_and_server(scene_tree, network, server_api, server_node)
+	_connect_to_server_api_functions(server_api, server_node)
+
+func _connect_network_and_server(scene_tree : SceneTree, network : ENetMultiplayerPeer, server_api : SceneMultiplayer, server_node : Node):
+	var ip = local_ip if ProjectSettings.get_setting("global/connect_to_local") else server_ip
+	var client_status = network.create_client(ip, port, max_channels)
+	_check_status(client_status, network)
+	server_api.multiplayer_peer = network
+	server_api.root_path = server_node.get_path()
+	scene_tree.set_multiplayer(server_api, server_node.get_path())
+	scene_tree.multiplayer_poll = false
+
+func _connect_to_server_api_functions(server_api : SceneMultiplayer, server_node : Node):
+	#server_api.connection_failed.connect(server_node._on_connection_failed)
+	#server_api.connected_to_server.connect(server_node._on_connection_succeeded)
+	server_api.peer_packet.connect(server_node._on_packet_from_server)
+
+func _check_status(client_status, network : ENetMultiplayerPeer) -> void:
+	if client_status != OK:
+		OS.alert("Client creation failed")
+	if network.get_connection_status() == MultiplayerPeer.CONNECTION_DISCONNECTED:
+		OS.alert("Failed to connect to the authintiaction server")
---
+++ RollbackAuthPlugins/addons/rollbackauthnetcode/rollback_auth.gd
@@ -0,0 +1,14 @@
+@tool
+extends EditorPlugin
+
+# The project must be exported, then the project can be imported in other projects to
+# share the code between the two of them.
+
+func _enter_tree() -> void:
+	# Initialization of the plugin goes here.
+	pass
+
+
+func _exit_tree() -> void:
+	# Clean-up of the plugin goes here.
+	pass
